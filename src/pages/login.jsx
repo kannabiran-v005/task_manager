24b65fbe978c6b57179945a8e0eac70afd680079
Kannabiran V <vkannabiran24bcs0215@iiitkottayam.ac.in>
2026-06-01 10:17:22 +0530

    Update login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -9,7 +9,7 @@ function Login() {
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post("http://localhost:5000/login", {
+      const res = await axios.post("https://task-manager-s08q.onrender.com/", {
         email,
         password,
       });
